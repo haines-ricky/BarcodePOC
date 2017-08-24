f8e0ff36e9cd79f9689085b6c8e9f65c47e4f639
Haines, Richard <richard.haines@us.sogeti.com>
2017-08-24 14:39:29 -0500

    Add project files.

---
+++ BarcodePOC/BarcodePOC/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodePOC
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void OnScanRequest(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            // Navigate to our scanner page
+            await Navigation.PushAsync(scanPage);
+        }
+
+        async void TestButton(object sender, EventArgs e)
+        {
+            await this.DisplayAlert(
+                    "OnScanRequest",
+                    "Barcode scan requested!",
+                    "Ok!");
+        }
+    }
+}
